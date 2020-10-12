7a1c07989b6a037d2f336519254abbb8050b006a
Vitaly Liber <zenamax@gmail.com>
2020-10-12 19:50:05 +0300

    project files was added

---
+++ Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    // public GameObject[] dots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Initialize()
+    {
+        // var dotToUse = Random.Range(0, dots.Length);
+        // var dot = Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        // dot.transform.SetParent(transform);
+        // dot.name = gameObject.name;
+    }
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+    public GameObject[] dots;
+    public GameObject[,] allDots;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (var i = 0; i < width; i++)
+        {
+            for (var j = 0; j < height; j++)
+            {
+                var tempPosition = new Vector2(i, j);
+                var backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+                backgroundTile.transform.SetParent(transform);
+                backgroundTile.name = "( " + i + ", " + j + " )";
+                var dotToUse = Random.Range(0, dots.Length);
+                var dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.SetParent(transform);
+                dot.name = "( " + i + ", " + j + " )";
+                allDots[i, j] = dot;
+            }
+        }
+    }
+}
